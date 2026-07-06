ca940d1f73881008752cacd2ab68acf430fd0cd3
jephthahdominic <stdominicjephthah63@gmail.com>
2026-07-06 13:28:49 +0100

    worked on changes

--- src/services/lecturer.service.ts
+++ src/services/lecturer.service.ts
@@ -40,12 +40,12 @@ class LecturerService {
       throw new AppError('Lecturer email already in use', HTTP_STATUS.CONFLICT);
     }
 
-    if (data.staffId) {
-      const staffIdExists = await lecturerRepository.findByStaffId(data.staffId);
-      if (staffIdExists) {
-        throw new AppError('Staff ID already in use', HTTP_STATUS.CONFLICT);
-      }
-    }
+    // if (data.staffId) {
+    //   const staffIdExists = await lecturerRepository.findByStaffId(data.staffId);
+    //   if (staffIdExists) {
+    //     throw new AppError('Staff ID already in use', HTTP_STATUS.CONFLICT);
+    //   }
+    // }
 
     const createData: Partial<ILecturerDocument> = {
       firstName: data.firstName,
